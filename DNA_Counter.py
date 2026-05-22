b2411e08a9399124d5aa4970977d148f7677e7f3
Ajay Kannan A <ajaykannan872@gmail.com>
2026-05-22 17:53:21 +0530

    Create DNA_Counter.py

---
+++ DNA_Counter.py
@@ -0,0 +1,12 @@
+dna_ipt = input("Enter: ").upper()
+
+A = dna_ipt.count("A")
+T = dna_ipt.count("T")
+G = dna_ipt.count("G")
+C = dna_ipt.count("C")
+
+print("A:", A)
+print("T:", T)
+print("G:", G)
+print("C:", C)
+print("Length:", len(dna_ipt))
